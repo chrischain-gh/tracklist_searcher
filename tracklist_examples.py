2d7b37d9cb62fafa382f73691b3fdda444ad72ea
chrischain-gh <69068010+chrischain-gh@users.noreply.github.com>
2022-02-18 01:44:31 -0500

    Tracklist examples

---
+++ tracklist_examples.py
@@ -0,0 +1,263 @@
+algorithm = "Line Split"
+tracklist_raw = """Ross 154 - Mayflower
+Y.S. - Magic Dome
+B.C. - Stronghold
+Chez Damier - Help Myself
+Kenny Larkin - Soul Man
+Sun Electric - Entrance (D. Angel Rmx)
+Dave Angel - Endless Motion
+St. Vitus Dance - Mystic Vibrations
+Forces - EssR'ay
+Dave Angel - Artech
+Josh Wink - Liquid Summer
+Dan Curtin - Track 17
+Dan Curtin - Voices From Another Age
+Sunrise Society - Northern Lights
+Paul Hazel - Test Pattern
+F2 - Dominica
+Ian Pooley - Twin Gods"""
+
+
+algorithm = "Line Split + \'# - Timestamp -\'"
+tracklist_raw = """01 - 00:00 - DJ Dainskin - Move Your Body
+02 - 05:21 - Dave Angel - Funk Music (Dave Angel Rework)
+03 - 09:24 - Ian Pooley - Gimmie Sound
+04 - 14:07 - X-Madman - Help
+05 - 18:17 - Ian Pooley presents Bluelite - Enlite
+06 - 23:33 - Dave Angel - Funk Music (DJ Tonka Mix)
+07 - 28:29 - Peace Division - D Tuned
+08 - 33:18 - Luke Slater - Stomp
+09 - 36:00 - DJ Restyles - Past Silence
+10 - 41:36 - Olav Basoski - Water
+11 - 44:48 - Mark Seven - Don't Let Me Down
+12 - 50:03 - Olav Basoski - Bring The Bass
+13 - 53:56 - Mark Seven - Gonz Gets FU
+14 - 56:22 - Jan Driver - Filter
+15 - 01:00:16 - Mark N-R-G - Don't Stop (Dave Angel Remix)
+16 - 01:04:45 - Kamaflarge - Discotamination (Dave Angel Rework)
+17 - 01:08:40 - Christian Smith vs Jean-Phillippe Aviance - Goldrush
+18 - 01:13:41 - Warped 69 - A High Naturally (Dave Angel Retake)
+01 - 01:18:54 - Spirit Guide - The First Few Steps
+02 - 01:23:20 - The Mulder - The Police
+03 - 01:26:23 - Jack Mackrel - Drum Tumbler
+04 - 01:28:50 - Velocity Boy - Triangular Funk
+05 - 01:32:41 - Bushflange - Foot Thought
+06 - 01:36:44 - Protein Boy - Wanna Go Bang
+07 - 01:40:22 - Olav Basoski - The Prize
+08 - 01:43:57 - Olav Basoski - Beats U Up
+09 - 01:46:58 - Olav Basoski - Keep On Dancin'
+10 - 01:49:26 - Ian Pooley - Loopduell 1 (On Guard Mix)
+11 - 01:52:26 - Dave Angel - This Is Disco (Dave Angel Remix)
+12 - 01:56:12 - Alexi Delano & Cari Lekebusch - Jimmy's Technique
+13 - 01:58:53 - 2 B Continued - Laydown (S'n'S Remix)
+14 - 02:03:43 - The Subjective - Tremmer
+15 - 02:05:57 - Mark Seven - Aftershock
+16 - 02:10:15 - Mark Seven - High
+17 - 02:14:43 - Nico Awtsventin - Jumping Jupiter
+18 - 02:18:07 - Green - Poison
+19 - 02:21:08 - Nico Awtsventin - Formulas
+20 - 02:26:00 - Adam Beyer - 4.50
+21 - 02:28:53 - Nico Awtsventin - Funx Patterns"""
+
+
+algorithm = "YouTube Licensing"
+tracklist_raw = """Song
+Ending Point
+Artist
+Conrad Van Orton
+Album
+Hypersomnia
+Licensed to YouTube by
+dig dis (on behalf of Attic Music)
+Song
+Kinetic Lullaby (Pris Remix)
+Artist
+AWB
+Album
+Kinetic Lullaby
+Licensed to YouTube by
+Triple Vision Record Distribution (on behalf of Taapion)
+Song
+Articulation
+Artist
+Altinbas
+Album
+Articulation
+Licensed to YouTube by
+DeepMiningCorpAssoc (on behalf of Float Records); IRICOM
+Song
+Derzhprom (Keith Carnal Remix)
+Artist
+Opuswerk
+Album
+Social Condenser
+Licensed to YouTube by
+Triple Vision Record Distribution (on behalf of ARTS); Muserk Rights Management, and 2 Music Rights Societies
+Song
+Black Swan
+Artist
+Dasha Rush
+Album
+Ain't No God Nor King
+Licensed to YouTube by
+DeepMiningCorpAssoc (on behalf of Sonic Groove)
+Song
+Script (I/Y Remix)
+Artist
+Sept
+Album
+Voice Of The Night Vol. 1
+Licensed to YouTube by
+Triple Vision Record Distribution (on behalf of Voxnox Records)
+Song
+In Basements (Original Mix)
+Artist
+Robert Hood
+Album
+Apartment Zero
+Licensed to YouTube by
+Zebralution, and 1 Music Rights Societies
+Song
+Citofono 1
+Artist
+CNCPT
+Album
+Evidence Based Vol. 4
+Licensed to YouTube by
+Triple Vision Record Distribution (on behalf of Triple Vision Music Group)
+Song
+You Are (Dark Mix)
+Artist
+Kenny Larkin
+Album
+You Are
+Licensed to YouTube by
+[Merlin] K7 Records (on behalf of Planet E Communications)
+Song
+Steady Pulse
+Artist
+Jeroen Search
+Album
+Endless Circles EP
+Licensed to YouTube by
+[Merlin] Was Wordandsound Medien GmbH (on behalf of Figure); Muserk Rights Management, and 2 Music Rights Societies"""
+
+
+algorithm = "Discogs #1"
+tracklist_raw = """1.1	Dance Fever–	Out On The Dancefloor
+Written-By – Karl Kowalski*
+Written-By – Karl Kowalski*
+1.2	Robert Armani–	Circus Bells (Hardfloor Remix)
+Remix – Hardfloor
+Written-By – Robert Armani
+Remix – Hardfloor
+Written-By – Robert Armani
+1.3	Laurent Garnier–	Moon Beam
+Written-By – Laurent Garnier
+Written-By – Laurent Garnier
+1.4	Brian Transeau–	Relativity (Carl Craig's Urban Affair Dub)
+Remix – Carl Craig
+Written-By – Brian Transeau, Carl Craig
+Remix – Carl Craig
+Written-By – Brian Transeau, Carl Craig
+1.5	H.M.C.* / A.S.I.O.*–	Tropic Of Capricorn
+Written-By – A.S.I.O.*, H.M.C.*
+Written-By – A.S.I.O.*, H.M.C.*
+1.6	Guillaume La Tortue–	Salinas
+Written-By – Guillaume La Tortue
+Written-By – Guillaume La Tortue
+1.7	Kenny Larkin–	Track
+Written-By – Kenny Larkin
+Written-By – Kenny Larkin
+1.8	Mike Dearborn–	Deviant Behaviour
+Written-By – Mike Dearborn
+Written-By – Mike Dearborn
+1.9	Deepside–	French
+Written-By – Ludovic Navarre
+Written-By – Ludovic Navarre
+1.10	Galaxy 2 Galaxy–	Journey Of The Dragons
+Written-By – Mad Mike*
+Written-By – Mad Mike*
+1.11	Rhythim Is Rhythim–	Icon
+Written-By – Derrick May
+Written-By – Derrick May
+1.12	Kenny Larkin–	Prelude
+Written-By – Kenny Larkin
+Written-By – Kenny Larkin
+1.13	Mazdaratti–	Excelsior Eod Fingered
+Written-By – Fred Gianelli*
+Written-By – Fred Gianelli*
+1.14	Essence Of Nature*–	Blue Orchidee
+Written-By – Ralf Hildenbeutel, Sven Väth
+Written-By – Ralf Hildenbeutel, Sven Väth
+1.15	Planetary Assault Systems–	Gated
+Written-By – Alan Sage, Luke Slater
+Written-By – Alan Sage, Luke Slater
+1.16	Underground Resistance–	The Final Frontier
+Written-By – Underground Resistance
+Written-By – Underground Resistance
+1.17	Dave Angel–	Original Man
+Written-By – Dave Angel
+Written-By – Dave Angel
+1.18	Laurent Garnier–	Go To Sleep
+Written-By – Laurent Garnier"""
+
+
+algorithm = "mixesdb1"
+tracklist_raw = """[000] Klaus Wiese - Tibetische Klangschalen 1 [Edition Ak]
+[0??] Wink - Full Late Night [Ovum]
+[0??] Edit Select - Hi Line Extraction (Mike Parker Version) [Lanthan]
+[0??] Antonio Ruscito - Immagini Dai Sogni (Edit Select Remix) [Edit Select]
+[0??] Jeff Rushin - Coda [Mote Evolver]
+[014] Pastaboys & Osunlade - Deep Musique (Trus'me Spritz Mix) [Rebirth]
+[018] Jeff Rushin - Enigma [Mote-Evolver]
+[0??] Juxta Position - Sweat Box [Mistress]
+[0??] Jeroen Search - Untitled [White]
+[0??] Museum Of Plate - Septem F [White]
+[0??] DJ Dozia - Pop Culture (Ambivalent's Culture Mix) [Ovum]
+[035] Spencer Parker - Rights For Men [Work Them]
+[0??] Shinedoe - Inside Your Mind [Intacto]
+[0??] MBC - Untitled Too [Lessismorecordings]
+[0??] Spencer Parker - Silly Club Song No. 2 [Rekids]
+[043] Yotam Avni - This Is How (Sterac Remix) [Ovum - OVM-259]
+[0??] Yotam Avni - This Is How [Ovum]
+[053] Planetary Assault Systems - Twelve [Peacefrog - PFG 008DA]
+[054] Mike Dunn - Dance You Mutha (Ju Edit) [White]
+[0??] Orgue Electronique Feat. Robert Owen - Our House (DJ TLR Remix) [Crème Organization]
+[0??] Cajmere Feat. Derrick Carter - Wet Dream (Ju Edit) [White]
+[068] Drew Sky - Pressure Point [Chiwax - CCE 004]
+[0??] Inner City - Ahnonghay (Carl Craig Remix) [Music Man]
+[0??] Fanon Flowers - From East To West Part 1 [Automat]
+[078] Orlando Voorn - In Da Jungle (Chocolate Puma Miami Cut) [Big & Dirty - BADRD 037]
+[081] Woody McBride - All Night (Techno Mix) [Subspec]
+[0??] Dax J - The Invisible Man [White]
+[0??] Stranger - Untitled (B1) [Paling Trax]
+[0??] Dirty Basscore - MBass [Credo]
+[095] Mikael Jonasson - Quatar (Audio Injection Remix) [Ground Factory - GF 049]
+[100] Mathew Jonson - Marionette (The Beginning) [Wagon Repair]
+[106] Fuse - Substance Abuse (Barem Remix) [Plus 8 - 100]
+[109] Cassegrain & Tin Man - Window Window [Infrastructure New York - INF 019]
+[113] Members Of Mayday - MOM [Electric Kingdom]
+[11?] Envoy - Dark Manoeuvres [Soma]
+[11?] Klaus Wiese - Tibetische Klangschalen 1 [Edition Ak]"""
+
+
+algorithm = "mixesdb2"
+tracklist_raw = """?
+Allen Ginsberg - America
+Amazing Farmer Singers Of Chicago - I Got A Telephone In My Bosom
+Orlando Julius And His Afro Sounders - Mura Sise
+Metropolis - The Gatekeeper
+Anders And - 00045 (Love)
+?
+Florist - Windows On The World (Moving Day Dub)
+?
+Mall Grab - Caught Slippin'
+Salary Boy - The Illusion
+Loods - Nobody
+Kikuchi Momoko - Blind Curve
+Jesse Futermans - A Tribute To Horace"""
+
+
+algorithm = "OCR"
+tracklist_raw = """test8.jpg"""
